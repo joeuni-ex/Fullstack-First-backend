53ca5087a5ded9ddc41985885309935a5f008115
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-26 14:02:39 +0900

    새유저 앤드포인트 user->users로 변경 및 getAllUsers(모든유저조회) 추가

--- backend/src/main/java/com/mysite/backend/controller/UserController.java
+++ backend/src/main/java/com/mysite/backend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.mysite.backend.controller;
 import com.mysite.backend.model.User;
 import com.mysite.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor//생성자 주입
@@ -15,10 +18,16 @@ public class UserController {
 
     //user로 요청할 때 requestbody에 담아서 보낸다
     //그리고 그 데이터를 userRepository에 저장한다.(db)
-    @PostMapping("/user")
+    @PostMapping("/users")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser); //저장하고 리턴되는 유저를 리턴함
 
+    }
+
+    //모든 유저 정보 가져오기
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
