c4d1bca0191f0bdb0f8268d702932537d7832ca3
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-27 15:12:12 +0900

    백엔드 파일

--- backend/src/main/java/com/mysite/backend/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.backend.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id) {
-        super("id에 맞는 유저가 없습니다." + id);
-    }
-}
